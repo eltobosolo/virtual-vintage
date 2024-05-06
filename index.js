20c7af5e049be3f03101523babfe8bf7f0d47db8
eltobosolo <eltobosolo@gmail.com>
2024-05-06 14:41:13 -0700

    try again

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const flagsmith = new Flagsmith({
 
 const flags = flagsmith.getEnvironmentFlags();
 
+var third_osc_enabled = flags.isFeatureEnabled("third_oscillator");
+
 const PORT = process.env.PORT || 5001
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index', { flags: flags } ))
+    .get('/', (req, res) => res.render('pages/index', { third_osc_enabled: third_osc_enabled } ))
     .get('/cool', (req, res) => res.send(cool()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
