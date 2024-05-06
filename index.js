ce39d4c98fb2310911225b023230bba756b6b2b9
eltobosolo <eltobosolo@gmail.com>
2024-05-06 13:54:19 -0700

    try again

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const Flagsmith = require('flagsmith-nodejs')
 
 const PORT = process.env.PORT || 5001
 
