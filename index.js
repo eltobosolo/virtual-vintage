784f825edb6344baa2404b951a90b037bed168fe
eltobosolo <eltobosolo@gmail.com>
2024-05-06 11:58:07 -0700

    Added flagsmith switch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const flagsmith = require('flagsmith-nodejs');
+
 
 const PORT = process.env.PORT || 5001
 
