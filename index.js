35390540add586886fbc1cccd3ddb457a48b5c3e
eltobosolo <eltobosolo@gmail.com>
2024-05-06 14:49:08 -0700

    try again

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const flagsmith = new Flagsmith({
     environmentKey: 'jhZAAroZixcZKDPDxh46Ek'
 });
 
-const flags = flagsmith.getEnvironmentFlags();
-
-var third_osc_enabled = flags.isFeatureEnabled("third_oscillator");
-
 const PORT = process.env.PORT || 5001
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index', { third_osc_enabled: third_osc_enabled } ))
+    .get('/', (req, res) => res.render('pages/index', { 
+      title: 'Coming Soon!', 
+      mainText: 'Eventually Podcast'
+    }))
     .get('/cool', (req, res) => res.send(cool()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
